4ebcc1d4ab971c48daad76fb12a4b379e7e48b45
Carlos Carreno <107624799+carlos-carreno@users.noreply.github.com>
2023-11-15 07:59:00 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿﻿﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Microsoft.Graph;
+using Newtonsoft.Json;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+using Microsoft.Kiota.Abstractions;
+
+
+namespace new_project
+{
+
+    public class ResponseData 
+    {
+        public string deviceId{get; set;}
+        public string policyName{get; set;}
+        public string displayName{get; set;}
+
+        public ResponseData(string deviceId, string policyName, string displayName) 
+        {
+            this.deviceId = deviceId;
+            this.policyName = policyName;
+            this.displayName = displayName;
+        }
+    }
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Toda esta seccion es la autenticacion a la aplicaicon cread en azure
+            string[] scopes = {"https://graph.microsoft.com/.default"};
+           // Credenciales de la aplicacion
+           var clientId = "2e01c288-3ae0-4386-b3fe-543eaad5e8ad";
+           var tenantId = "f47dcce4-02e7-4d88-a659-a2d0bbe170e7";
+           var clientSecret = "kPI8Q~sqwjtA10jDL1G4WUbp6LbncNo.RgtfGa3X";
+           // using Azure.Identity;
+           var options = new ClientSecretCredentialOptions
+           {
+            AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+            };
+            var clientSecretCredential = new ClientSecretCredential(
+                tenantId, clientId, clientSecret, options);
+
+            //var responseList = new List<ResponseData>();
+                //consulta de microsoft graph
+            var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+            try
+            {
+                var devices = await graphClient.Devices.GetAsync((requestConfiguration) =>
+                {
+                    requestConfiguration.QueryParameters.Filter = "startswith(operatingSystem,'Win') and profileType eq 'RegisteredDevice' and isManaged eq true and accountEnabled eq true";
+                });
+                Console.WriteLine(devices.Value.Count);
+
+            }
+            catch (Exception  ex)
+            {
+                Console.WriteLine($"Processing failed:{ex.Message}");
+            }
+            
+            
+             
+        }
+    } 
+} 
--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# codigo_pruebas_msgraph
+# Microsoft Graph API Example
+
+This is a sample C# program that demonstrates how to authenticate and use the Microsoft Graph API to retrieve device compliance policy states for devices. 
+
+## Prerequisites
+
+Before running this code, you need to have the following:
+
+- An Azure application registered with the required permissions to access Microsoft Graph.
+- .NET Core SDK installed.
+- Azure SDK for .NET installed.
+- Visual Studio or another C# development environment.
+
+## Setup
+
+1. Clone or download this repository to your local machine.
+2. Open the project in your C# development environment.
+3. Replace the following placeholders in the code with your own values:
+    - `clientId`: Your Azure AD application's client ID.
+    - `tenantId`: Your Azure AD tenant ID.
+    - `clientSecret`: Your Azure AD application's client secret.
+4. Build and run the application.
+
+## How the Code Works
+
+- The code uses the Azure Identity library to authenticate with Azure AD.
+- It uses the Microsoft.Graph library to interact with the Microsoft Graph API.
+- It retrieves a list of devices and their compliance policy states.
+- For each device, it creates a JSON object with the device ID and policy name and prints it to the console.
+
+## Error Handling
+
+The code includes error handling to capture and display any exceptions that occur during the process.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+## Acknowledgments
+
+- [Azure Identity library](https://github.com/Azure/azure-sdk-for-net/tree/main/sdk/identity/Azure.Identity)
+- [Microsoft Graph SDK for .NET](https://github.com/microsoftgraph/msgraph-sdk-dotnet)
+
+## Author
+
+[Your Name]
+
+## Contact
+
+For any questions or feedback, feel free to contact me at [Your Email Address].
